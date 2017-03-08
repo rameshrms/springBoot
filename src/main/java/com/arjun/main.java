97b000094c9bc9aa47c88d702f8cac532ef7ba5a
arjun <rapakaarjun@gmail.com>
2017-03-08 19:08:36 +0530

    test commit

--- src/main/java/com/arjun/main.java
+++ src/main/java/com/arjun/main.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by Arjun on 1/23/2017.
+ * tested commit
  */
 @SpringBootApplication
 public class main {
     public static void main(String[] args) {
         SpringApplication.run(main.class,args);
+        
     }
 }
